e67ee079af5f59ebb9d31d3a80ed2111fafc9315
Ionel-Cristinel Putinica <ionutputinicabz@gmail.com>
2020-04-10 19:44:30 +0200

    Added Camera Controller with scroll function

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //change size to zoom in/out the view
+    //clamp values to min and max
+    //use the mouse wheel scroll to controll the zoom
+
+    private void LateUpdate()
+    {
+        Camera.main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
