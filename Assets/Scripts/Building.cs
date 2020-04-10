3238d7377dd87e974ea26a03d23473fa08e756b1
Ionel-Cristinel Putinica <ionutputinicabz@gmail.com>
2020-04-10 19:08:22 +0200

    Created script for buildings + added building textures/materials

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building : MonoBehaviour
+{
+    //an integer which defines the id of a building - road=0, house=1, farm=2, factory=3
+    public int id;
+    //allows to define in the inspector the cost of buildings
+    public int cost;
+    public string buildingName;
+}
