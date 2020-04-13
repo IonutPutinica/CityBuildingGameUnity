0c8d2adb62ba92b8ab54aff5bdaff71f7d152343
Ionel-Cristinel Putinica <ionutputinicabz@gmail.com>
2020-04-13 15:09:30 +0200

    Created BuildingHandler class + assigned the controls the proper values

---
+++ Assets/Scripts/BuildingHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingHandler : MonoBehaviour
+{
+
+    [SerializeField]
+    private City city;
+    [SerializeField]
+    private UIController uiController;
+    [SerializeField]
+    //this array will store the prefabs and their according values
+    private Building[] buildings;
+    [SerializeField]
+    private Board board;
+    private Building selectedBuilding;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EnableBuilder(int building)
+    {
+        selectedBuilding = buildings[building];
+    }
+}
